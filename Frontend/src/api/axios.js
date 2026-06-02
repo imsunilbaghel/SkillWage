94cbd3db070764eff7a40fc38d1914c227a99129
Sunil Kumar Baghel <80190365+imsunilbaghel@users.noreply.github.com>
2026-06-02 22:23:57 +0530

    Api EndPoint Fixed

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const getBaseURL = () => {
-  if (typeof window !== "undefined" && window.location) {
-    const { hostname } = window.location;
-    return `http://${hostname}:8080/api`;
-  }
+  // if (typeof window !== "undefined" && window.location) {
+  //   const { hostname } = window.location;
+  //   return `http://${hostname}:8080/api`;
+  // }
   return import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 };
 
